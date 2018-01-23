924b6b29b24b56e36ba1a3e60846d786bd3793fb
RohitMagdum-VSI <rohit@voidstarindia.com>
2018-01-23 21:57:08 +0530

    Rohitmagdum (#11)
    * Added the possible scenes enums.
    
    Tried to break the scene into as much granularity as possible. This is
    not enough but is a start. Further more values will be added.
    
    * converted tabs into spaces and removed trailing numbers after enums in the last commit
    
    * Just adding my code and framework I use.
    
    Guys just adding the framework I have been using along with one test
    project. If anything from this is helpful please use. Any comments
    related to the same are most welcome.
    
    * Added handling of vertex, normals, tex coord buffers
    
    Changed the handling of Vertex, Normals and Tex Coords buffer. Keeping
    them member of derived class and base class agnostic of the data.

---
+++ prototypes/rohit_magdum/VSIDemos/VSIUtilPlus/VSIUtilTexture.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <SOIL.h>
+#include <gl\glew.h>
+#include <gl\GL.h>
+#include <gl\GLU.h>
+#include <VSIUtilPlus.h>
+
+namespace VSIUtil
+{
+	int VSIUtilPlus::VSIUtilLoadTexture(char* imageFile)
+	{
+		GLvoid* image;
+		int iRetVal = 0;
+		int width, height;
+		/* load an image file directly as a new OpenGL texture */
+
+		image = SOIL_load_image
+		(
+			imageFile,
+			&width,
+			&height,
+			0,
+			SOIL_LOAD_RGBA
+		);
+		if (image == NULL)
+		{
+			const char* ch = SOIL_last_result();
+			return -1;
+		}
+		glGenTextures(1, &(mTexture[0]));
+		glBindTexture(GL_TEXTURE_2D, mTexture[0]);
+		glTexStorage2D(GL_TEXTURE_2D, 8, GL_RGB8, width, height);
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, image);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		return 0;
+	}
+}
